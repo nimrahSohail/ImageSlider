bca351db3bac493a6c47335c0715315a25db87e2
nimrahSohail <102296918+nimrahSohail@users.noreply.github.com>
2022-10-08 00:11:42 +0500

    first commit

---
+++ imageslider/src/assets/index.js
@@ -0,0 +1,9 @@
+export const productImages = [
+    require('./images/3.jpg'),
+    require('./images/4.jpg'),
+    require('./images/5.jpg'),
+    require('./images/6.jpg'),
+    require('./images/7.jpg'),
+    require('./images/8.jpg'),
+    require('./images/9.jpg')
+]
